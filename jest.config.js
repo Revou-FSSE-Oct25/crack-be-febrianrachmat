03348aa703fb54fc4fed2cec108b22b766e1bdec
Rachmat Febrian <febrian.rachmat11@gmail.com>
2026-05-11 11:13:03 +0700

    chore(jest): exclude integration spec from default unit test run

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
   testRegex: '.*\\.spec\\.ts$',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '\\.integration\\.spec\\.ts$'],
   transform: {
     '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
